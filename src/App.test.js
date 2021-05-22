c9574f2c5f699b053f75744e8fe3876a8cf91502
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-05-22 16:58:18 +0200

    Started a test suite; wrote 2 tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { shallow } from "enzyme";
+import PersonList from "./Components/PersonList";
+
+/* 
+--------THIS IS THE INITIAL TEST THAT COMES WITH JEST WHEN WE USE CREATE REACT APP--------
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+}); */
+
+// 1. CREATE A TEST THAT FAILS
+// --- the keyword "describe" breaks your test suite into components
+// --- a test suite is a collection of tests
+describe("App", () => {
+  // --- "it" signals the beginning of our (first) test
+  it("", () => {
+    const getAppComponentWrapper = shallow(<App />); // shallow only loads the App component, no children
+  });
+
+  it("renders a component called PersonList", () => {
+    const getAppComponentWrapper = shallow(<App />);
+    const personList = getAppComponentWrapper.find(PersonList);
+
+    expect(personList).toHaveLength(1);
+  });
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,9 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+// we configure the adapter so that Jest can comunicate with the react app we are trying to test, using enzyme
+configure({ adapter: new Adapter() });
