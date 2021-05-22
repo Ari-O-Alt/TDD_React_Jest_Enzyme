f3c2916c497d51ea042ede1f6e0505e4e7bb3e9c
Arika O <11184750+Ari-O-Alt@users.noreply.github.com>
2021-05-22 18:46:40 +0200

    Created more tests in the PersonList test suite

--- src/Components/PersonList/PersonList.js
+++ src/Components/PersonList/PersonList.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-export default function PersonList() {
-  return (
-    <div>
-      <ul>
-        <li></li>
-      </ul>
-    </div>
-  );
-}
+const PersonList = (props) => {
+  const { people } = props;
+
+  const allPeople = people?.map((person, index) => {
+    return <li key={index}>{person.name}</li>;
+  });
+
+  if (people && (people.lenght = 0)) {
+    return <ul></ul>;
+  }
+
+  return <ul>{allPeople}</ul>;
+};
+
+export default PersonList;
--- src/Components/PersonList/PersonList.test.js
+++ src/Components/PersonList/PersonList.test.js
@@ -4,10 +4,39 @@ import PersonList from "./PersonList";
 
 // --- we create a test suite for this component (PersonList)
 describe("PersonList", () => {
+  // --- test that checks if the PersonList component is rendered and it it contains an unordered list
   it("renders an UL element", () => {
-    const getPersonListWrapper = shallow(<PersonList />); // we check if the component renders
-    const peopleListUls = getPersonListWrapper.find("ul"); // find an unordered list inside the component
+    const getPersonListWrapper = shallow(<PersonList />);
+    const peopleListUls = getPersonListWrapper.find("ul");
 
-    expect(peopleListUls).toHaveLength(1); // we expect to find just one list
+    expect(peopleListUls).toHaveLength(1); // we expect to find just one list since find returns an collection
+  });
+
+  // --- test that checks if no list items are displayed when people array pop is empty
+  it("displays no LIs", () => {
+    const getPersonListWrapper = shallow(<PersonList people={[]} />);
+    const peopleInTheList = getPersonListWrapper.find("li"); // returns an collection
+
+    expect(peopleInTheList).toHaveLength(0);
+  });
+
+  // --- test that checks if one list item is displayed when people array pop has an item
+  it("displays one LIs", () => {
+    const getPersonListWrapper = shallow(
+      <PersonList people={[{ name: "Alan" }]} />
+    );
+    const peopleInTheList = getPersonListWrapper.find("li"); // returns a collection
+
+    expect(peopleInTheList).toHaveLength(1);
+  });
+
+  // --- test that checks if the number of list items displayed is equal to the lenght of the "people" array
+  it("displays all LIs", () => {
+    const getPersonListWrapper = shallow(
+      <PersonList people={[{ name: "Alan" }, { name: "Jane" }]} />
+    );
+    const peopleInTheList = getPersonListWrapper.find("li"); // returns a collection
+
+    expect(peopleInTheList).toHaveLength(peopleInTheList.length);
   });
 });
